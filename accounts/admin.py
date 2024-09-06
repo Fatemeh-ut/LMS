8cf63c343b0c5b28517e0e8d0edf5d10f7c3e666
= <=>
2024-09-06 11:07:11 +0330

    the models and admin have complete

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Users
 # Register your models here.
+
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'password', 'email', 'first_name', 'last_name', 'role', 'is_active')
+    search_fields = ('username', 'first_name', 'last_name', 'role')
+
+
+admin.site.register(Users, UsersAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+class Users(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('borrower', 'Borrower'),
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='borrower')
+    groups = models.ManyToManyField(Group, related_name='users_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='users_permissions_set', blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
